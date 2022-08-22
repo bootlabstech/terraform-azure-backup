680d9b74b9ee6799e0f66640a0914470e2f93369
naveenkumar sivarajan <sivarajan.naveenkumar@mahindra.com>
2022-08-22 18:59:50 +0530

    added azure terraform codebase for backup-vm

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "azurerm_recovery_services_vault" {
+  value = azurerm_recovery_services_vault.services_vault
+}
+
+output "azurerm_backup_policy_vm" {
+  value = azurerm_backup_policy_vm.backup_policy
+}
