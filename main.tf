fb88e0425b6f9f742f11f4a1a9365b0fc7a42dc1
naveenkumar sivarajan <sivarajan.naveenkumar@mahindra.com>
2022-08-22 21:08:51 +0530

    added azure terraform codebase for backup-vm

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "azurerm_recovery_services_vault" "services_vault" {
-  name                     = var.services_vault_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  sku                      = "Standard"
+  name                = var.services_vault_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
 }
 
 resource "azurerm_backup_policy_vm" "backup_policy" {
-  name                     = var.backup_policy_name
-  resource_group_name      = var.resource_group_name
-  recovery_vault_name      = azurerm_recovery_services_vault.services_vault.name
+  name                = var.backup_policy_name
+  resource_group_name = var.resource_group_name
+  recovery_vault_name = azurerm_recovery_services_vault.services_vault_name
 
   backup {
     frequency = "Daily"
@@ -17,13 +17,13 @@ resource "azurerm_backup_policy_vm" "backup_policy" {
 }
 
 data "azurerm_virtual_machine" "vm" {
-  name                     = var.vm_name
-  resource_group_name      = var.resource_group_name
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_backup_protected_vm" "vm1" {
-  resource_group_name      = var.resource_group_name
-  recovery_vault_name      = azurerm_recovery_services_vault.services_vault.name
-  source_vm_id             = data.azurerm_virtual_machine.vm.id
-  backup_policy_id         = azurerm_backup_policy_vm.vm1.id
+  resource_group_name = var.resource_group_name
+  recovery_vault_name = azurerm_recovery_services_vault.services_vault_name
+  source_vm_id        = data.azurerm_virtual_machine.vm.id
+  backup_policy_id    = azurerm_backup_policy_vm.backup_policy.id
 }
